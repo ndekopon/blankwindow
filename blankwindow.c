960098f9801edc543ce723451f68897431425dd6
ndekopon <92087784+ndekopon@users.noreply.github.com>
2021-10-18 11:35:49 +0900

    最初のコミット

---
+++ blankwindow.c
@@ -0,0 +1,82 @@
+﻿#define WINVER 0x0A00
+#define _WIN32_WINNT 0x0A00
+
+#include <SDKDDKVer.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+const WCHAR window_title[] = L"blankwindow";
+const WCHAR window_class[] = L"blankwindowclass";
+
+LRESULT CALLBACK window_proc(HWND _window, UINT _message, WPARAM _wparam, LPARAM _lparam)
+{
+    switch (_message)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(_window, _message, _wparam, _lparam);
+    }
+    return 0;
+}
+
+inline ATOM _register_class(HINSTANCE _instance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = window_proc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = _instance;
+    wcex.hIcon = LoadIconW(NULL, IDI_APPLICATION);
+    wcex.hCursor = LoadCursorW(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = window_class;
+    wcex.hIconSm = LoadIconW(NULL, IDI_APPLICATION);
+
+    return RegisterClassExW(&wcex);
+}
+
+inline BOOL _init_instance(HINSTANCE _instance)
+{
+    HWND window = CreateWindowW(window_class, window_title, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, 360, 240, NULL, NULL, _instance, NULL);
+
+    if (!window)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(window, SW_NORMAL);
+    UpdateWindow(window);
+
+    return TRUE;
+}
+
+int WinMainCRTStartup(void)
+{
+    HINSTANCE instance = GetModuleHandleW(NULL);
+
+    _register_class(instance);
+
+    if (!_init_instance(instance))
+    {
+        return 1;
+    }
+
+    MSG msg;
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
